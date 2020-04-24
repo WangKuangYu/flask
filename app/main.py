d03cec7219d1c70ae13c67aebba66d96a8d9f16c
unknown <justice61990@gmail.com>
2020-04-24 16:50:12 +0800

    second

--- app/main.py
+++ app/main.py
@@ -43,3 +43,39 @@ def basic_templates2():
         "age":37
     }
     return render_template("basic.html", **output)
+
+@app.route('/try-qs')
+def queryString():
+    # query string 轉成 dict
+    # http://localhost:5000/try-qs?a[]=1&b=34&a[]=5
+    output = {
+        'args': request.args,
+        'a[]': request.args.getlist('a[]'),
+        'get_b': request.args.get('b'),
+        'get_a[]': request.args.get('a[]'),
+    }
+    return output
+
+@app.route('/try-post', methods=['POST'])  # 限定使用 POST
+def try_post():
+    # 表單資料 urlencoded, form-data 皆可, 使用 postman 測試
+    return request.form
+
+@app.route('/try-post2', methods=['POST'])
+def try_post2():
+    # 使用 postman post json 資料: {"a":11,"b":22}
+    output = {
+        'content_type': request.content_type,
+        'data': request.data.decode('utf-8'),
+        'json': request.get_json(),
+    }
+    return output
+
+@app.route('/params/')
+@app.route('/params/<action>/')
+@app.route('/params/<action>/<int:id>')
+def my_params(action='none', id=0):
+    return ( {
+        'action': action,
+        'id': id
+    } )
